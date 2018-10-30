d823e52d3fcda2e6ba92b8b84d2a6674a99556c0
Niko Salonen <niko.salonen@almamedia.fi>
2018-10-30 15:03:37 +0200

    update comma-danging

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,9 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>Junia jee</h1>
-    <p>https://rata.digitraffic.fi/api/v1/live-trains/station/HKI?arrived_trains=5&arriving_trains=5&departed_trains=5&departing_trains=5&include_nonstopping=false</p>
+    <p>
+      https://rata.digitraffic.fi/api/v1/live-trains/station/HKI?arrived_trains=5&arriving_trains=5&departed_trains=5&departing_trains=5&include_nonstopping=false
+    </p>
 
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
