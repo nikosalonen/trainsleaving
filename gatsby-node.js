011cb5c2420b4ed6c4d0179f23a7d1b172f38006
Niko Salonen <niko.salonen@almamedia.fi>
2018-11-01 11:34:47 +0200

    Add axios call for trains

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,46 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const axios = require(`axios`)
+const crypto = require(`crypto`)
 
-// You can delete this file if you're not using it
+exports.sourceNodes = async ({ boundActionCreators }) => {
+  const { createNode } = boundActionCreators
+
+  // fetch raw data from the Train api
+  const fetchTrain = () => axios.get(`https://rata.digitraffic.fi/api/v1/live-trains/station/HKI?arrived_trains=5&arriving_trains=5&departed_trains=5&departing_trains=5&include_nonstopping=false`)
+  // await for results
+  const res = await fetchTrain()
+  // map into these results and create nodes
+  res.data.map((train, i) => {
+    // Create your node object
+    const trainNode = {
+      // Required fields
+      id: `${i}`,
+      parent: `__SOURCE__`,
+      internal: {
+        type: `Train`, // name of the graphQL query --> allTrain {}
+        // contentDigest will be added just after
+        // but it is required
+      },
+      children: [],
+
+      // Other fields that you want to query with graphQl
+      trainCategory: train.trainCategory,
+      commuterLineID: train.commuterLineID,
+      cancelled: train.cancelled,
+
+
+    }
+
+    // Get content digest of node. (Required field)
+    const contentDigest = crypto.
+      createHash(`md5`).
+      update(JSON.stringify(trainNode)).
+      digest(`hex`)
+    // add it to userNode
+    trainNode.internal.contentDigest = contentDigest
+
+    // Create node with the gatsby createNode() API
+    createNode(trainNode)
+  })
+
+
+}
