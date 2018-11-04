0fb89d8b5d81ee0efbdd23fa751acab0e51bdd64
Niko Salonen <nikotsalonen@gmail.com>
2018-11-04 18:24:50 +0200

    remove obsolete stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,40 +8,14 @@ import Settings from '../components/settings'
 import Layout from '../components/layout'
 
 const IndexPage = () => {
-  // const trains = props.data.allTrain.edges
   return (
     <Layout>
       <Time />
       <Settings />
       <Trains />
-      {/* {trains.map((train, i) => {
-        const trainData = train.node
-        if (trainData.commuterLineID.length) {
-          return (
-            <div key={i}>
-              <p>Category: {trainData.trainCategory}</p>
-              <p>LineID: {trainData.commuterLineID}</p>
-            </div>
-          )
-        }
-      })} */}
-      {/* <Link to="/page-2/">Go to page 2</Link> */}
     </Layout>
   )
 }
-
-// export const query = graphql`
-//   query RandomUserQuery {
-//     allTrain {
-//       edges {
-//         node {
-//           trainCategory
-//           commuterLineID
-//         }
-//       }
-//     }
-//   }
-// `
 
 IndexPage.propTypes = {
   data: PropTypes.object,
