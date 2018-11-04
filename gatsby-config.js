90d74e8dfbdcb5dce4e5e64c3bba9ce1436d9ac5
Niko Salonen <nikotsalonen@gmail.com>
2018-11-04 10:14:34 +0200

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Lähtevät junat`
+    title: `Lähtevät junat`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,16 +11,16 @@ module.exports = {
         exclude: /(node_modules|cache|public)/,
         options: {
           emitWarning: true,
-          failOnError: false
-        }
-      }
+          failOnError: false,
+        },
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -28,14 +28,34 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Lähtevät junat`,
-        short_name: `lähtevät`,
+        short_name: `Lähtevät`,
         start_url: `/`,
         background_color: `#0078ca`,
         theme_color: `#0078ca`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
     },
-    `gatsby-plugin-offline`
-  ]
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-128638877-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [ `/preview/**`, `/do-not-track/me/too/` ],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: `lahtevat.info`,
+      },
+    },
+  ],
 }
