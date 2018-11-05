c8f1cb99821db9a8e632af56a72e59fbb89044f3
Niko Salonen <niko.salonen@almamedia.fi>
2018-11-05 08:58:40 +0200

    update ga config

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,13 +42,13 @@ module.exports = {
       options: {
         trackingId: `UA-128638877-1`,
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
-        anonymize: true,
+        anonymize: false,
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: [ `/preview/**`, `/do-not-track/me/too/` ],
+        // exclude: [ `/preview/**`, `/do-not-track/me/too/` ],
         // Enables Google Optimize using your container Id
         // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
         // Any additional create only fields (optional)
