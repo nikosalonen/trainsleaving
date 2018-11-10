e059bf36550a96cafad77a69571be027e0a845ce
Niko Salonen <nikotsalonen@gmail.com>
2018-11-10 18:59:49 +0200

    comment out styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,6 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import Ad from './ad'
 
 import 'spectre.css'
 
@@ -85,12 +84,12 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
 
         <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
+        // style={{
+        //   margin: `0 auto`,
+        //   maxWidth: 960,
+        //   padding: `0px 1.0875rem 1.45rem`,
+        //   paddingTop: 0,
+        // }}
         >
           {children}
         </div>
