977f55a8f5119a6e5529f5c22bc3485474e23a32
Niko Salonen <nikotsalonen@gmail.com>
2018-11-10 18:26:20 +0200

    ads

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,24 +26,49 @@ const Layout = ({ children }) => (
         <Helmet title={data.site.siteMetadata.title}>
           <html lang="fi" />
           <meta name="title" content={data.site.siteMetadata.title} />
-          <meta name="description" content={data.site.siteMetadata.description} />
+          <meta
+            name="description"
+            content={data.site.siteMetadata.description}
+          />
           <meta name="og:title" content={data.site.siteMetadata.title} />
           <meta name="og:url" content={data.site.siteMetadata.url} />
-          <meta name="og:image" content={data.site.siteMetadata.thumbnail && data.site.siteMetadata.thumbnail} />
-          <meta name="og:image:secure_url" content={data.site.siteMetadata.thumbnail && data.site.siteMetadata.thumbnail} />
-          <meta name="og:description" content={data.site.siteMetadata.description} />
+          <meta
+            name="og:image"
+            content={
+              data.site.siteMetadata.thumbnail &&
+              data.site.siteMetadata.thumbnail
+            }
+          />
+          <meta
+            name="og:image:secure_url"
+            content={
+              data.site.siteMetadata.thumbnail &&
+              data.site.siteMetadata.thumbnail
+            }
+          />
+          <meta
+            name="og:description"
+            content={data.site.siteMetadata.description}
+          />
           <meta name="og:image:width" content={1200} />
           <meta name="og:image:height" content={630} />
           <meta name="twitter:card" content={`summary_large_image`} />
           <meta name="twitter:title" content={data.site.siteMetadata.title} />
-          <meta name="twitter:description" content={data.site.siteMetadata.description} />
-          <meta name="twitter:image" content={data.site.siteMetadata.thumbnail && data.site.siteMetadata.thumbnail} />
+          <meta
+            name="twitter:description"
+            content={data.site.siteMetadata.description}
+          />
+          <meta
+            name="twitter:image"
+            content={
+              data.site.siteMetadata.thumbnail &&
+              data.site.siteMetadata.thumbnail
+            }
+          />
           <meta name="robots" content={`index, follow`} />
           <meta name="twitter:creator" content={`@nikosalonen`} />
           <meta name="og:site_name" content={`lahtevat.info`} />
-
         </Helmet>
-
 
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
