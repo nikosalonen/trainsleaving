1039b85a2a2f674f32c646e8223b5c0f0135cf8d
Niko Salonen <niko.salonen@almamedia.fi>
2018-11-12 10:30:44 +0200

    feedback with captcha

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -8,7 +8,7 @@ const Feedback = () => (
     <div className="container">
       <div className="columns">
         <div className=" column col-12">
-          <form name="contact" method="POST" netlify>
+          <form name="contact" method="POST" netlify data-netlify="true" data-netlify-honeypot="bot-field" action="/thanks/">
             <div className="form-group">
               <label className="form-label" htmlFor="name">Nimi:</label>
               <input className="form-input" type="text" id="name" placeholder="Nimi" />
@@ -21,7 +21,9 @@ const Feedback = () => (
               <label className="form-label" htmlFor="message">Viesti:</label>
               <textarea className="form-input" id="message" name="message"></textarea>
             </div>
+            <div data-netlify-recaptcha></div>
             <div className="form-group">
+              <input type="hidden" name="form-name" value="contact" />
               <button className="btn" type="submit">Lähetä</button>
             </div>
 
