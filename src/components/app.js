e6af00da0e0e848f96d8597608f4dbde1694ee71
Niko Salonen <niko.salonen@almamedia.fi>
2019-11-25 15:13:09 +0200

    restore ad

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,7 @@ import localStorage from 'localStorage'
 import 'spectre.css'
 import './layout.css'
 import ReactFitText from 'react-fittext'
-// import Ads from './ads'
+import Ads from './ads'
 import { AppProvider } from '../context/context'
 
 class App extends React.Component {
@@ -381,7 +381,9 @@ class App extends React.Component {
     return (
       <div className="app container">
         <div className="columns">
-
+          <div className="column col-12">
+            <Ads client={`ca-pub-0748442567853482`} slot={`4470981255`} />
+          </div>
           <div className="column col-12 col-ml-auto text-right">
             <Time />
           </div>
